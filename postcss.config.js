8ed4c545471409ede3e3050790b4e1f584a87574
Muhammad Muhajir <muhajirframe@gmail.com>
2019-06-10 21:12:39 +0700

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const production = !process.env.ROLLUP_WATCH;
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+module.exports = {
+  plugins: [
+    require("postcss-import")(),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    production &&
+      purgecss({
+        content: ["./**/*.html", "./**/*.svelte"],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  ]
+};
---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<svelte:options tag="my-custom-element" />
+<script>
+  export let name;
+</script>
+
+<div class="bg-red-500">
+  <h1>Hello {name}!</h1>
+</div>
+
+<style>
+  @import "/bundle.css";
+</style>
